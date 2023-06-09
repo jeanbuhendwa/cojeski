ebff0aa60ba66664f7a984647b0e2ec8fb082677
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-06-09 15:39:41 +0200

    Add pages

---
+++ src/pages/Campaign.js
@@ -0,0 +1,11 @@
+import React from "react";
+const CampaignsPage = () => {
+  return (
+    <div>
+      <h1>Campaigns</h1>
+      <p>This is the campaigns page</p>
+    </div>
+  );
+};
+
+export default CampaignsPage;
---
+++ src/pages/Programs.js
@@ -0,0 +1,11 @@
+import React from "react";
+const ProgramsPage = () => {
+  return (
+    <div>
+      <h1>Programs Page</h1>
+      <p>This is the programs page</p>
+    </div>
+  );
+};
+
+export default ProgramsPage;
